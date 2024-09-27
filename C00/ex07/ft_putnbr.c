10e000b5b2cd9a7dcc0e79b2da0232b8f4fea109
LionessLilo <leandra@lionesslilo.com>
2024-09-27 11:02:13 +0200

    🚧 Worked on ex07

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: lilo <lilo@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/09/25 11:47:32 by lilo          #+#    #+#                 */
-/*   Updated: 2024/09/25 17:51:36 by lilo          ########   odam.nl         */
+/*   Updated: 2024/09/27 11:00:12 by lilo          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,20 @@ void	ft_putnbr(int nb)
 	int		number;
 	int		remainder;
 	char	print_number;
+	char	unit;
+	char	print_unit;
 
 	number = nb;
 	while (number / 10)
 	{
 		remainder = number % 10;
 		print_number = remainder + '0';
-		write(1, &print_number, 1);
+		write(0, &print_number, 1);
 		number = number / 10;
-		// printf("dbug: %d\n", number);
 	}
+	unit = number % 10;
+	print_unit = unit + '0';
+	write(0, &print_unit, 1);
 }
 
 int	main(void)
