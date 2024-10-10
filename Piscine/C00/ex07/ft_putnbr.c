c4d29c076e4a4c9660fad6318ada7c76ad59113d
LionessLilo <63396227+LionessLilo@users.noreply.github.com>
2024-10-10 18:43:17 +0200

    🚧 Update ft_putnbr.c

--- Piscine/C00/ex07/ft_putnbr.c
+++ Piscine/C00/ex07/ft_putnbr.c
@@ -11,31 +11,58 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+
+int	is_min_int(int nm)
+{
+	if (nm == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		exit(0);
+	}
+	if (nm < 0)
+	{
+		nm = -nm;
+		write(1, "-", 1);
+	}
+	return (nm);
+}
+
+void	write_array(int i, char add_to_array[i])
+{
+	while (i >= 0)
+	{
+		write(1, &add_to_array[i], 1);
+		i--;
+	}
+}
 
 void	ft_putnbr(int nb)
 {
-	int		number;
 	int		remainder;
 	char	print_number;
-	char	unit;
-	char	print_unit;
+	char	add_to_array[10];
+	int		i;
 
-	number = nb;
-	while (number / 10)
+	nb = is_min_int(nb);
+	i = 0;
+	while (nb / 10)
 	{
-		remainder = number % 10;
+		remainder = nb % 10;
 		print_number = remainder + '0';
-		write(0, &print_number, 1);
-		number = number / 10;
+		add_to_array[i] = print_number;
+		i++;
+		nb = nb / 10;
 	}
-	unit = number % 10;
-	print_unit = unit + '0';
-	write(0, &print_unit, 1);
+	remainder = nb % 10;
+	print_number = remainder + '0';
+	add_to_array[i] = print_number;
+	write_array(i, add_to_array);
 }
 
 int	main(void)
 {
-	ft_putnbr(321);
+	ft_putnbr(2147483647);
 	return (0);
 }
