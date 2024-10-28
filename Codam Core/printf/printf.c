16b5931b4c911c26bc99b499838ea0caa31a5ea5
Leandra Lourens <leandra@lionesslilo.com>
2024-10-28 15:39:53 +0100

    🎉 Opened my printf and started trying to figure things out

---
+++ Codam Core/printf/printf.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   printf.c                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: llourens <llourens@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/10/28 15:06:52 by llourens      #+#    #+#                 */
+/*   Updated: 2024/10/28 15:20:12 by llourens      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_printf(const char *, ...)
+{
+	va_list list;
+
+	va_end(list);
+}
+// %[flag][min width][precision][length modifier][conversion specifier]
