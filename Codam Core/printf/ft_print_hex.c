1a6389c71826ef34e9eb3d67a8914a042887ad98
llourens <llourens@f0r3s7.codam.nl>
2024-11-02 16:30:42 +0100

    🚧 Did more work on the hex

--- Codam Core/printf/ft_print_hex.c
+++ Codam Core/printf/ft_print_hex.c
@@ -6,23 +6,40 @@
 /*   By: llourens <llourens@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/10/31 14:03:31 by llourens      #+#    #+#                 */
-/*   Updated: 2024/11/01 15:23:30 by llourens      ########   odam.nl         */
+/*   Updated: 2024/11/02 16:29:18 by lilo          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 int	ft_print_hex(unsigned long nb)
 {
 	int		remainder;
-	char *	hex_string;
+	char	*hex_string;
 
 	if (nb >= 16)
 	{
-		nb = ft_print_hex(nb / 16);
-		remainder = ft_print_hex(nb % 16);
-		if (remainder <= 9)
-			{
-				hex_string = remainder + '0';
-			}
+		// nb = ft_print_hex(nb / 16);
+		// remainder = ft_print_hex(nb % 16);
 	}
-	if (nb <= 9)
+	else
+	{
+		if (nb <= 9)
+		{
+			printf("%ld", nb);
+		}
+	}
+	// if (remainder <= 9)
+	// 	ft_print_number(remainder);
+	// if (remainder > 9 && remainder <= 15)
+
+	return (0);
+}
+int	main(void)
+{
+	printf("\nhex: %x\n", 6);
+	ft_print_hex(6);
+
+	printf("\nhex: %x\n", 10);
+	ft_print_hex(10);
 }
--- Codam Core/printf/ft_print_number.c
+++ Codam Core/printf/ft_print_number.c
@@ -6,7 +6,7 @@
 /*   By: llourens <llourens@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/10/31 14:30:11 by llourens      #+#    #+#                 */
-/*   Updated: 2024/11/01 13:06:19 by llourens      ########   odam.nl         */
+/*   Updated: 2024/11/02 16:11:57 by lilo          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ int	ft_print_number(int nbr)
 
 	str_to_print = ft_itoa(nbr);
 	count = ft_printstr(str_to_print);
-	printf("print_number count: %d");
 	return (count);
 }
 
