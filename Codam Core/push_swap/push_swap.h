26c1aeac06840f12dec0a85a7df726da00391abb
Leandra <leandra@lionesslilo.com>
2024-12-19 17:23:29 +0100

    🎉 Started push_swap

---
+++ Codam Core/push_swap/push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   push_swap.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: root <root@student.codam.nl>                 +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/12/19 16:14:08 by root          #+#    #+#                 */
+/*   Updated: 2024/12/19 17:09:50 by root          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+char	*push_swap(char *stack_a);
+
+#endif
