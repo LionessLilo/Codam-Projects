a53f2e017fe87004bef1fcb4b2c62a85a8eded00
Leandra <leandra@lionesslilo.com>
2024-12-22 15:50:29 +0100

    🚧 Started on push_swap

--- Codam Core/push_swap/srcs/main.c
+++ Codam Core/push_swap/srcs/main.c
@@ -1,22 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   push_swap.h                                        :+:    :+:            */
+/*   main.c                                             :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: root <root@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2024/12/19 16:14:08 by root          #+#    #+#                 */
-/*   Updated: 2024/12/19 17:09:50 by root          ########   odam.nl         */
+/*   Created: 2024/12/22 15:28:23 by root          #+#    #+#                 */
+/*   Updated: 2024/12/22 15:49:13 by root          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+// #include "../printf/ft_printf.h"
+// #include "../printf/libft/libft.h"
+#include <unistd.h>
+#include <stdlib.h>
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
-# endif
+int	ft_isvalid_arg(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		write(2, "No arguments found", 18);
+		return (0);
+	}
+	
+	return (1);
+}
 
-char	*push_swap(char *stack_a);
+int	main(int argc, char *argv[])
+{
+	int	valid;
 
-#endif
+	valid = ft_isvalid_arg(argc, argv);
+	if (valid == 0)
+		exit ;
+	else 
+
+}
