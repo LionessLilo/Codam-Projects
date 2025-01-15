f35a3b3ef6665e9c5207207862fe18e28bcf3abd
Leandra Lourens <leandra@lionesslilo.com>
2025-01-15 21:21:12 +0100

    📝 Revision     - Append node

---
+++ Codam Core/push_swap/Revision_code/append_node.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   append_node.c                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: llourens <llourens@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/01/15 21:14:01 by llourens      #+#    #+#                 */
+/*   Updated: 2025/01/15 21:19:53 by llourens      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+//Write a function append_node(t_node **head, int value) to add a new node to the end of a linked list.
+#include "revision.h"
+
+void	append_node(t_stack_node *node, int	content)
+{
+	t_stack_node	new_node;
+	
+	if (!node)
+		return ;
+		
+}
