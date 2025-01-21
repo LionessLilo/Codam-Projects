452fee6fa9d416fedfe2e3b2e9953b9a0abb1dc7
Leandra Lourens <leandra@lionesslilo.com>
2025-01-21 13:00:23 +0100

    🚧 Operations and tests      - rotate      - reverse rotate

--- Codam Core/push_swap/utils/reverse_rotate_operations.c
+++ Codam Core/push_swap/utils/reverse_rotate_operations.c
@@ -1,20 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   rotate_operations.c                                :+:    :+:            */
+/*   reverse_rotate_operations.c                        :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: llourens <llourens@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2025/01/21 10:22:28 by llourens      #+#    #+#                 */
-/*   Updated: 2025/01/21 12:11:28 by llourens      ########   odam.nl         */
+/*   Created: 2025/01/21 12:34:44 by llourens      #+#    #+#                 */
+/*   Updated: 2025/01/21 12:59:04 by llourens      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 #include "../printf/ft_printf.h"
-#include <stdio.h>
 
-static void	rotate(t_stack_node **head)
+void	reverse_rotate(t_stack_node **head)
 {
 	t_stack_node	*first_node;
 	t_stack_node	*second_node;
@@ -22,37 +21,43 @@ static void	rotate(t_stack_node **head)
 
 	if (!head || !(*head) || !(*head)->ptr_next)
 	{
-		ft_printf("Not enough nodes to rotate");
+		ft_printf("Not enough nodes to reverse rotate");
 		return ;
 	}
 	first_node = *head;
 	second_node = (*head)->ptr_next;
 	last_node = ft_lstlast(*head);
-	*head = second_node;
-	second_node->ptr_prev = NULL;
-	first_node->ptr_prev = last_node;
-	first_node->ptr_next = NULL;
+	*head = last_node;
+	last_node->ptr_prev = NULL;
 	last_node->ptr_next = first_node;
+	first_node->ptr_prev = last_node;
+	second_node->ptr_next = NULL;
 }
 
-void	ra(t_stack_node **stack_a, int checker)
+void rra(t_stack_node **stack_a, int checker)
 {
-	rotate(stack_a);
+	reverse_rotate(stack_a);
 	if (checker == 0)
-		printf("ra\n");
+		ft_printf("rra\n");
 }
 
-void	rb(t_stack_node **stack_b, int checker)
+void rrb(t_stack_node **stack_b, int checker)
 {
-	rotate(stack_b);
+	reverse_rotate(stack_b);
 	if (checker == 0)
-		printf("rb\n");
+		ft_printf("rrb\n");
 }
 
-void	rr(t_stack_node **stack_a, t_stack_node **stack_b, int checker)
+void rrb(t_stack_node **stack_b, int checker)
 {
-	rotate(stack_a);
-	rotate(stack_b);
+	reverse_rotate(stack_b);
 	if (checker == 0)
-		printf("rr\n");
+		ft_printf("rrb\n");
+}
+
+void rrr(t_stack_node **stack_a, t_stack_node **stack_b, int checker)
+{
+	reverse_rotate(stack_b);
+	if (checker == 0)
+		ft_printf("rrr\n");
 }
