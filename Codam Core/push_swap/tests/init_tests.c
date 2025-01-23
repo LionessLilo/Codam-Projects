80017308a722433d443f5b86c948f488838616c0
Leandra Lourens <leandra@lionesslilo.com>
2025-01-23 11:24:51 +0100

    ✅  Created Initialization tests for negatve numbers      - With 3 items in a stack      - With 10 items in a stack

--- Codam Core/push_swap/tests/init_tests.c
+++ Codam Core/push_swap/tests/init_tests.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/01/20 14:32:23 by root          #+#    #+#                 */
-/*   Updated: 2025/01/22 21:15:28 by root          ########   odam.nl         */
+/*   Updated: 2025/01/23 11:20:49 by llourens      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -126,7 +126,81 @@ int	main(void)
 		log_tick();
 	else
 		printf("\033[0;31mError\033[0;37m");
-	// #pragma endregion
+	#pragma endregion
+
+	#pragma region initialisation with negative numbers and 3 items
+	a_node = NULL;
+	char *argv3[] = {"4", "-3", "1", NULL};
+	fprintf(fd, "Argv: \n");
+	i = 0;
+	while (argv3[i])
+	{
+		fprintf(fd, "Arg[%d]: %s\n", i, argv3[i]);
+		i++;
+	}
+	// Act
+	init_stack_a(&a_node, argv3);
+	// Assert
+	fprintf(fd, "\n");
+	fprintf(fd, "Nodes in the stack: \n");
+	temp = a_node;
+	while (temp)
+	{
+		fprintf(fd, "%d\n", temp->int_nbr);
+		temp = temp->ptr_next;
+	}
+	temp = NULL;
+	fprintf(fd, "\n");
+	printf("\033[0;37mInitialise stack 3 items: ");
+	if (a_node->int_nbr == ft_atol(argv3[0]) && 
+		a_node->ptr_next->int_nbr == ft_atol(argv3[1]) && 
+		a_node->ptr_next->ptr_next->int_nbr == ft_atol(argv3[2]))
+		log_tick();
+	else
+		fprintf(fd, "Error");
+
+	#pragma endregion
+
+	#pragma region initialisation with negative numbers and 10 items
+	a_node = NULL;
+	fprintf(fd, "Argv list: \n");
+	char *argv4[] = {"6",  "3", "1", "-2", "5", "16", "-45", "-7", "-9", "17", "8", NULL};
+	fprintf(fd, "Argv: \n");
+	i = 0;
+	while (argv4[i])
+	{
+		fprintf(fd, "Arg[%d]: %s\n", i, argv2[i]);
+		i++;
+	}
+	init_stack_a(&a_node, argv4);
+	fprintf(fd, "\n");
+	fprintf(fd, "Nodes in the stack: \n");
+	temp = a_node;
+	while (temp)
+	{
+		fprintf(fd, "%d\n", temp->int_nbr);
+		temp = temp->ptr_next;
+	}
+	temp = NULL;
+	fprintf(fd, "\n");
+	printf("\033[0;37mInitialise stack 10 items: ");
+	if (a_node->int_nbr == ft_atol(argv4[0]) && 
+		a_node->ptr_next->int_nbr == ft_atol(argv4[1]) && 
+		a_node->ptr_next->ptr_next->int_nbr == ft_atol(argv4[2]) &&
+		a_node->ptr_next->ptr_next->ptr_next->int_nbr == ft_atol(argv4[3]) &&
+		a_node->ptr_next->ptr_next->ptr_next->ptr_next->int_nbr == ft_atol(argv4[4]) &&
+		a_node->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->int_nbr == ft_atol(argv4[5]) &&
+		a_node->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->int_nbr == ft_atol(argv4[6]) &&
+		a_node->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->int_nbr == ft_atol(argv4[7]) &&
+		a_node->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->int_nbr == ft_atol(argv4[8]) &&
+		a_node->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->int_nbr == ft_atol(argv4[9]) &&
+		a_node->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->ptr_next->int_nbr == ft_atol(argv4[10]))
+
+		log_tick();
+	else
+		printf("\033[0;31mError\033[0;37m");
+
+	#pragma endregion
 
 	fclose(fd);
 	free_stack(&a_node);
