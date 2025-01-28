e1af3238f8335a5c58249eecd2ad5896b3825ae8
Leandra Lourens <leandra@lionesslilo.com>
2025-01-28 09:25:17 +0100

    🐛 Sorts but not within 500 operations.

--- Codam Core/push_swap/src/05_radix_sort.c
+++ Codam Core/push_swap/src/05_radix_sort.c
@@ -6,38 +6,69 @@
 /*   By: llourens <llourens@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/01/27 15:45:11 by llourens      #+#    #+#                 */
-/*   Updated: 2025/01/27 17:44:45 by llourens      ########   odam.nl         */
+/*   Updated: 2025/01/28 09:24:41 by llourens      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 #include "../printf/ft_printf.h"
 
-void	radix_sort(t_stack_node **stack_a, t_stack_node **stack_b, int stack_len)
+void push_numbers(t_stack_node **stack_a, t_stack_node **stack_b, int stack_len, int current_bit_position, int *numbers_pushed, int *rotations)
 {
-    int	current_bit_position;
-    int	processed_nbrs;
+    int processed_nbrs = 0;
 
-    if (!stack_a || !*stack_a)
+    while (processed_nbrs < stack_len)
     {
-        ft_printf("Error: stack_a is null\n");
-        return;
-    }
-    assign_indices(stack_a);
-    current_bit_position = 0;
-    while (current_bit_position < 32)
-    {
-        processed_nbrs = 0;
-        while (processed_nbrs < stack_len)
+        if (((*stack_a)->int_index_in_sorted_array >> current_bit_position) & 1)
         {
-            if (((*stack_a)->int_index_in_sorted_array >> current_bit_position) & 1)
-                ra(stack_a, 0);
-            else
-                pb(stack_a, stack_b, 0);
-            processed_nbrs++;
+            ra(stack_a, 0);
+            (*rotations)++;
         }
-        while (*stack_b)
-            pa(stack_a, stack_b, 0);
+        else
+        {
+            pb(stack_a, stack_b, 0);
+            (*numbers_pushed)++;
+        }
+        processed_nbrs++;
+    }
+}
+
+void restore_stack(t_stack_node **stack_a, t_stack_node **stack_b, int numbers_pushed, int rotations)
+{
+    while (numbers_pushed > 0)
+    {
+        pa(stack_a, stack_b, 0);
+        numbers_pushed--;
+    }
+    while (rotations > 0)
+    {
+        rra(stack_a, 0);
+        rotations--;
+    }
+}
+
+void process_numbers(t_stack_node **stack_a, t_stack_node **stack_b, int stack_len, int current_bit_position)
+{
+    int numbers_pushed = 0;
+    int rotations = 0;
+
+    push_numbers(stack_a, stack_b, stack_len, current_bit_position, &numbers_pushed, &rotations);
+    restore_stack(stack_a, stack_b, numbers_pushed, rotations);
+}
+
+void radix_sort(t_stack_node **stack_a, t_stack_node **stack_b, int stack_len)
+{
+    int current_bit_position;
+
+    if (!stack_a || !*stack_a || stack_len <= 1)
+        return;
+
+    assign_indices(stack_a); 
+    current_bit_position = 0;
+
+    while ((1 << current_bit_position) < stack_len)
+    {
+        process_numbers(stack_a, stack_b, stack_len, current_bit_position);
         current_bit_position++;
     }
 }
