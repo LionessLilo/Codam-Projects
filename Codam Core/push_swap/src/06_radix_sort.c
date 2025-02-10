5ad7663220fba789c1e1192f4673fd8f77f8aa9f
Leandra Lourens <leandra@lionesslilo.com>
2025-02-10 14:23:06 +0100

    🐛  Done the best combination I can between least number of moves and it actually sorting.

--- Codam Core/push_swap/src/06_radix_sort.c
+++ Codam Core/push_swap/src/06_radix_sort.c
@@ -6,7 +6,7 @@
 /*   By: llourens <llourens@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/01/28 13:02:11 by llourens      #+#    #+#                 */
-/*   Updated: 2025/02/10 14:05:37 by llourens      ########   odam.nl         */
+/*   Updated: 2025/02/10 14:19:26 by llourens      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,57 +14,29 @@
 #include "../printf/ft_printf.h"
 #include "../printf/libft/libft.h"
 
-void	radix_sort(t_stack_node **stack_a, 
+void	radix_sort(t_stack_node **stack_a,
 			t_stack_node **stack_b, int stack_len)
 {
 	int	current_bit_position;
 	int	processed_nbrs;
-	t_stack_node	*largest_nbr;
 
 	if (!stack_a || !*stack_a)
-	{
-		ft_putendl_fd("Error", 2);
-		return ;
-	}
-	largest_nbr = find_highest(*stack_a);
+		return (ft_putendl_fd("Error", 2));
 	assign_indices(stack_a);
 	current_bit_position = 0;
-	if (largest_nbr->int_nbr < 99999)
+	while (current_bit_position < 9)
 	{
-		while (current_bit_position < 7)
+		processed_nbrs = 0;
+		while (processed_nbrs++ < stack_len)
 		{
-			processed_nbrs = 0;
-			while (processed_nbrs < stack_len)
-			{
-				if (((*stack_a)->int_index_in_sorted_array
-						>> current_bit_position) & 1)
-					ra(stack_a, 0);
-				else
-					pb(stack_a, stack_b, 0);
-				processed_nbrs++;
-			}
-			while (*stack_b)
-				pa(stack_a, stack_b, 0);
-			current_bit_position++;
+			if (((*stack_a)->int_index_in_sorted_array
+					>> current_bit_position) & 1)
+				ra(stack_a, 0);
+			else
+				pb(stack_a, stack_b, 0);
 		}
-	}
-	else
-	{
-		while (current_bit_position < 10)
-		{
-			processed_nbrs = 0;
-			while (processed_nbrs < stack_len)
-			{
-				if (((*stack_a)->int_index_in_sorted_array
-						>> current_bit_position) & 1)
-					ra(stack_a, 0);
-				else
-					pb(stack_a, stack_b, 0);
-				processed_nbrs++;
-			}
-			while (*stack_b)
-				pa(stack_a, stack_b, 0);
-			current_bit_position++;
-		}
+		while (*stack_b)
+			pa(stack_a, stack_b, 0);
+		current_bit_position++;
 	}
 }
