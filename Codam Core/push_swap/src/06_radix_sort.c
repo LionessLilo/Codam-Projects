f7fd3c3d91102dde11005dc8295bf0b455b8fe7a
Leandra Lourens <leandra@lionesslilo.com>
2025-02-11 13:48:16 +0100

    🚀 Made evaluation ready changes      - Changed the radix sort function to be Norminette complient      - Created a number generator      - Removed unnecessary files

--- Codam Core/push_swap/src/06_radix_sort.c
+++ Codam Core/push_swap/src/06_radix_sort.c
@@ -6,7 +6,7 @@
 /*   By: llourens <llourens@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/01/28 13:02:11 by llourens      #+#    #+#                 */
-/*   Updated: 2025/02/10 23:22:44 by root          ########   odam.nl         */
+/*   Updated: 2025/02/11 13:44:32 by llourens      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,28 @@
 #include "../printf/ft_printf.h"
 #include "../printf/libft/libft.h"
 
-// void	radix_sort(t_stack_node **stack_a,
-// 			t_stack_node **stack_b, int stack_len)
-// {
-// 	int	current_bit_position;
-// 	int	processed_nbrs;
+static void	process_stack(t_stack_node **stack_a, t_stack_node **stack_b,
+		int stack_len, int current_bit_position)
+{
+	int	processed_nbrs;
 
-// 	if (!stack_a || !*stack_a)
-// 		return (ft_putendl_fd("Error", 2));
-// 	assign_indices(stack_a);
-// 	current_bit_position = 0;
-// 	while (current_bit_position < 7)
-// 	{
-// 		processed_nbrs = 0;
-// 		while (processed_nbrs < stack_len)
-// 		{
-// 			if (((*stack_a)->int_index_in_sorted_array
-// 					>> current_bit_position) & 1)
-// 				ra(stack_a, 0);
-// 			else
-// 				pb(stack_a, stack_b, 0);
-// 			processed_nbrs++;
-// 		}
-// 		while (*stack_b)
-// 			pa(stack_a, stack_b, 0);
-// 		current_bit_position++;
-// 	}
-// }
+	processed_nbrs = 0;
+	while (processed_nbrs < stack_len)
+	{
+		if (((*stack_a)->int_index_in_sorted_array >> current_bit_position) & 1)
+			ra(stack_a, 0);
+		else
+			pb(stack_a, stack_b, 0);
+		processed_nbrs++;
+	}
+	while (*stack_b)
+		pa(stack_a, stack_b, 0);
+}
 
-void	radix_sort(t_stack_node **stack_a, t_stack_node **stack_b, int stack_len)
+void	radix_sort(t_stack_node **stack_a,
+			t_stack_node **stack_b, int stack_len)
 {
 	int	current_bit_position;
-	int	processed_nbrs;
 	int	max_bits;
 
 	if (!stack_a || !*stack_a)
@@ -57,17 +47,7 @@ void	radix_sort(t_stack_node **stack_a, t_stack_node **stack_b, int stack_len)
 		max_bits++;
 	while (current_bit_position < max_bits)
 	{
-		processed_nbrs = 0;
-		while (processed_nbrs < stack_len)
-		{
-			if (((*stack_a)->int_index_in_sorted_array >> current_bit_position) & 1)
-				ra(stack_a, 0);
-			else
-				pb(stack_a, stack_b, 0);
-			processed_nbrs++;
-		}
-		while (*stack_b)
-			pa(stack_a, stack_b, 0);
+		process_stack(stack_a, stack_b, stack_len, current_bit_position);
 		current_bit_position++;
 	}
 }
