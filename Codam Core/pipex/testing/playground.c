eecc5d6efebf9f7baf0483495a76f9975923f79b
Leandra Lourens <leandra@lionesslilo.com>
2025-02-13 14:43:14 +0100

    💩 Playing pid's in playground.

--- Codam Core/pipex/testing/playground.c
+++ Codam Core/pipex/testing/playground.c
@@ -6,17 +6,24 @@
 /*   By: llourens <llourens@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/02/11 18:04:06 by llourens      #+#    #+#                 */
-/*   Updated: 2025/02/12 18:17:53 by llourens      ########   odam.nl         */
+/*   Updated: 2025/02/13 14:37:26 by llourens      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 int	main(void)
 {
-	int	fd[2];
+	pid_t	pid;
 
-	
+	pid = getpid();
+	printf("pid before fork: %d\n", pid);
+	pid = fork();
+	printf("first pid after fork: %d\n", pid);
+
+	return (2);
 }
+
