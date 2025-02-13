883395b77ca4ac90b9f1c0ada097d5ba6bf4ec43
Leandra Lourens <leandra@lionesslilo.com>
2025-02-13 18:55:40 +0100

    💩 Played with forking in forking_playground

--- Codam Core/pipex/testing/forking_playground.c
+++ Codam Core/pipex/testing/forking_playground.c
@@ -6,7 +6,7 @@
 /*   By: llourens <llourens@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/02/11 18:04:06 by llourens      #+#    #+#                 */
-/*   Updated: 2025/02/13 16:39:08 by llourens      ########   odam.nl         */
+/*   Updated: 2025/02/13 18:46:12 by llourens      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 int	main(void)
 {
@@ -21,16 +22,18 @@ int	main(void)
 	int		fd[2];
 
 	pid = getpid();
-
 	printf("pid before fork: %d\n", pid);
 	pid = fork();
 	printf("first pid after fork: %d\n", pid);
 
 	if (pid == 0)
-		printf("Child pid: %d\n", pid);
+		printf("This is the child\n");
 
 	else
-		printf("Parent pid: %d\n", pid);
+	{
+		wait(NULL);
+		printf("This is the parent\n");
+	}
 
 	return (2);
 }
