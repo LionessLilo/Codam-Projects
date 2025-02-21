cbc0308a01d64e6fb32b7c3ab12e0b9217dc2a5c
Leandra Lourens <leandra@lionesslilo.com>
2025-02-21 17:32:17 +0100

    🚧 It reads from a file on one end of the pipe and writes to a file on the other end of the pipe.

--- Codam Core/pipex/src/input_and_cmd1.c
+++ Codam Core/pipex/src/input_and_cmd1.c
@@ -6,7 +6,7 @@
 /*   By: llourens <llourens@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/02/17 14:48:11 by llourens      #+#    #+#                 */
-/*   Updated: 2025/02/21 15:45:27 by llourens      ########   odam.nl         */
+/*   Updated: 2025/02/21 16:43:37 by llourens      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,11 @@ void	input_and_cmd1(char **input_file, int pipe_fd_write, int pipe_fd_read)
 	char	*fetched_input;
 
 	close(pipe_fd_read);
-	if (input_file == NULL || *input_file == NULL)
-		perror_and_exit("Input file is NULL");
 	input_file_fd = open(*input_file, O_RDONLY);
 	fetched_input = get_next_line(input_file_fd);
 	if (fetched_input == NULL)
 		perror_and_exit("Failed to read from file1");
-	write(pipe_fd_write, fetched_input, (ft_strlen(fetched_input) + 1));
+	write(pipe_fd_write, fetched_input, ft_strlen(fetched_input));
 	close(pipe_fd_write);
 	close(input_file_fd);
 }
--- Codam Core/pipex/src/output_and_cmd2.c
+++ Codam Core/pipex/src/output_and_cmd2.c
@@ -6,7 +6,7 @@
 /*   By: llourens <llourens@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/02/17 17:24:29 by llourens      #+#    #+#                 */
-/*   Updated: 2025/02/20 09:36:18 by llourens      ########   odam.nl         */
+/*   Updated: 2025/02/21 17:25:49 by llourens      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,18 @@ void	output_and_cmd2(int pipe_fd_write, int pipe_fd_read)
 {
 	char	buffer[128];
 	int		bytes_read;
+	int		fd_output_file;
 
 	close(pipe_fd_write);
+	fd_output_file = open("./output_file.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd_output_file < 0)
+	{
+		perror("open");
+		exit(EXIT_FAILURE);
+	}
 	bytes_read = read(pipe_fd_read, buffer, 12);
-	write(1, &buffer, 12);
+	dup2(STDIN_FILENO, fd_output_file);
+	write(fd_output_file, &buffer, 12);
 	buffer[bytes_read] = '\0';
 	close(pipe_fd_read);
 }
