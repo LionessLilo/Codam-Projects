599d361e36e809d5299a0638b5a50ade78b66953
Leandra Lourens <leandra@lionesslilo.com>
2025-03-19 17:22:07 +0100

    🚧 Did some work on the algorithm.

--- Codam Core/fractol/fractol.h
+++ Codam Core/fractol/fractol.h
@@ -6,7 +6,7 @@
 /*   By: llourens <llourens@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/03/13 09:15:31 by llourens      #+#    #+#                 */
-/*   Updated: 2025/03/19 12:51:21 by llourens      ########   odam.nl         */
+/*   Updated: 2025/03/19 16:48:50 by llourens      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,30 @@
 # include "./printf/ft_printf.h"
 # include "./printf/libft/libft.h"
 # include <stdlib.h>
-# include <aio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <math.h>
 
 # define WIDTH 1000
 # define HEIGHT 1000
+# define MAX_ITERATIONS 1000
 
+typedef struct s_complex_nbr
+{
+	double	real;
+	double	imaginary;
+
+}			t_complex_nbr;
+
+typedef struct s_coordinates
+{
+	double	x;
+	double	y;
+
+}			t_coordinates;
+
+/*Mandelbrot*/
 void	create_mandelbrot_image(mlx_t mandelbrot_window);
+int		mandelbrot_equation(t_coordinates coordinates);
 
 #endif
--- Codam Core/fractol/src/mandelbrot.c
+++ Codam Core/fractol/src/mandelbrot.c
@@ -6,27 +6,65 @@
 /*   By: llourens <llourens@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/03/13 15:55:33 by llourens      #+#    #+#                 */
-/*   Updated: 2025/03/19 12:50:39 by llourens      ########   odam.nl         */
+/*   Updated: 2025/03/19 17:16:23 by llourens      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fractol.h"
 
+int	mandelbrot_equation(t_coordinates coordinates);
+
 void	create_mandelbrot_image(mlx_t mandelbrot_window)
 {
-	mlx_image_t	*img;
+	mlx_image_t		*img;
+	t_coordinates	coordinates;
+	int				iterations;
 
+	coordinates.x = WIDTH * 0.1;
+	coordinates.y = HEIGHT * 0.1;
+	iterations = 0;
 	img = mlx_new_image(&mandelbrot_window, WIDTH, HEIGHT);
-	int	y = 1000 * 0.1;
-	while (y < (1000 * 0.9))
+	while (coordinates.y < (HEIGHT * 0.9))
 	{
-		int x = 1000 * 0.1;
-		while (x < (1000 * 0.9))
+		coordinates.x = 0;
+		while (coordinates.x < (WIDTH * 0.9))
 		{
-			mlx_put_pixel(img, x, y, (rand() % 0x1000000));
-			x++;
+			iterations = mandelbrot_equation(coordinates);
+			if (iterations < MAX_ITERATIONS)
+				mlx_put_pixel(img, coordinates.x, coordinates.y, 0xFF0000);
+			if (iterations == MAX_ITERATIONS)
+				mlx_put_pixel(img, coordinates.x, coordinates.y, 0x00FF00);
+			coordinates.x++;
 		}
-		y++;
+		coordinates.y++;
 	}
 	mlx_image_to_window(&mandelbrot_window, img, 0, 0);
+}
+
+int	mandelbrot_equation(t_coordinates coordinates)
+{
+	t_complex_nbr	complex_nbr;
+	double			temp;
+	int				i;
+	double			temp_real;
+	double			temp_imag;
+
+	i = 0;
+	temp = 0;
+	complex_nbr.real = coordinates.x;
+	complex_nbr.imaginary = coordinates.y;
+	while ((complex_nbr.real * complex_nbr.real)
+		+ (complex_nbr.imaginary * complex_nbr.imaginary)
+		<= 4 && i < MAX_ITERATIONS)
+	{
+		temp_real = complex_nbr.real * complex_nbr.real
+			- complex_nbr.imaginary * complex_nbr.imaginary
+			+ complex_nbr.real;
+		temp_imag = 2 * complex_nbr.real * complex_nbr.imaginary
+			+ complex_nbr.imaginary;
+		complex_nbr.real = temp_real;
+		complex_nbr.imaginary = temp_imag;
+		i++;
+	}
+	return (i);
 }
