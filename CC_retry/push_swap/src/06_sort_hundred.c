04d814df6abe0a9401745a82e9c235d9ec9148da
llourens <llourens@f0r3s7.codam.nl>
2025-09-19 16:17:19 +0200

    🚧 Started work on 100 numbers chunk sorting.

---
+++ CC_retry/push_swap/src/06_sort_hundred.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   06_sort_hundred.c                                  :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lilo <lilo@student.codam.nl>                 +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/09/19 16:13:39 by lilo          #+#    #+#                 */
+/*   Updated: 2025/09/19 16:16:41 by lilo          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+void	sort_five(t_stack_node **stack_a, t_stack_node **stack_b, int len)
+{
+	assign_indices(stack_a);
+}
